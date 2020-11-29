38c2d0be9ca5f90402b219c3fadf96d78af923ed
Hannah Brown <2hbrown13@gmail.com>
2020-11-29 10:27:19 -0600

    adding reimbursement controller

--- ProjectOne/src/main/java/com/revature/servlets/MainServlet.java
+++ ProjectOne/src/main/java/com/revature/servlets/MainServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class EmployeeLoginServlet extends HttpServlet {
+public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
