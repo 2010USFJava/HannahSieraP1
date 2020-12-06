4be34a737cdaac3c553a5012504542905c6c2e86
Hannah Brown <2hbrown13@gmail.com>
2020-12-06 15:55:17 -0600

    adding reimbursement servlet

--- ProjectOne/src/main/java/com/revature/servlets/ViewReimbursementServlet.java
+++ ProjectOne/src/main/java/com/revature/servlets/ViewReimbursementServlet.java
@@ -36,16 +36,27 @@ public class ViewReimbursementServlet extends HttpServlet {
 	             Statement stmt = conn.createStatement();  
 	             ResultSet rs = stmt.executeQuery("select * from reimbursement");  
 	             out.println("<table border=1 width=50% height=50%>");  
-	             out.println("<tr><th>Reimbursement ID</th><th>Employee ID</th><th>Description</th><tr>");  
+	             out.println("<tr><th>Reimbursement ID</th><th>Employee ID</th><th>Event Date</th><th>Request Date</th><th>Location</th><th>Amount</th><th>Description</th><th>Event Type</th><th>Grade Type</th><th>Status</th><tr>");  
 	             while (rs.next()) 
 	             {  
 	                 int a = rs.getInt("reimbursement_id");  
 	                 int b = rs.getInt("employee_id");  
-	                 String c = rs.getString("description");   
-	                 out.println("<tr><td>" + a + "</td><td>" + b + "</td><td>" + c + "</td></tr>");   
+	                 String c = rs.getString("event_date");
+	                 String d = rs.getString("request_date");
+	                 String e = rs.getString("location");
+	                 int f = rs.getInt("amount");
+	                 String g = rs.getString("description");
+	                 String h = rs.getString("event_type");
+	                 String i = rs.getString("grade_type");
+	                 out.println("<tr><td>" + a + "</td><td>" + b + "</td><td>" + c + "</td><td>" + d + "</td><td>" + e + "</td><td>"+ f +"</td><td>" + g +"</td><td>"+ h +"</td><td>"+ i +"</td></tr>");   
 	             }  
 	             out.println("</table>");  
-	             out.println("</html></body>");  
+	             out.println("<div>");
+
+	             out.println("<a href=\"http://localhost:8080/ProjectOne/resources/html/profile.html\"> Back </a>");
+	             out.println("</div>");
+	             out.println("</html></body>");
+	             
 	             conn.close();  
 	            }  
 	             catch (Exception e) 
